12742219d6878384130b86382f723b36a9822d2b
Jon Jaques <jon@3five.com>
2015-08-04 12:09:21 -0500

    Get basic theme implemented

---
+++ index.js
@@ -0,0 +1,9 @@
+var base = 'node_modules/3five-shopify-theme'
+
+module.exports.assets = [
+  base + '/assets/fonts/**/*',
+  base + '/assets/glyphs/**/*',
+  base + '/assets/images/**/*',
+]
+
+module.exports.base = base
